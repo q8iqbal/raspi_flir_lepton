9a8eb58eda193fcf00bd59626fdc397611a6153d
iqbal nur imansyah <iqbalnurimansyah@gmail.com>
2022-03-30 21:16:15 +0700

    tes

--- lepton/lepton_i2c.py
+++ lepton/lepton_i2c.py
@@ -3,10 +3,10 @@ from lib import lepton_sdk, lepton_sys, lepton_oem, lepton_type
 class LeptonI2c :
     def __init__(self):
         self._connected = False
-        self._port = lepton_type.LEP_CAMERA_PORT_DESC_T()
+        self._port = lepton_type.LEP_CAMERA_PORT_DESC_T_TAG()
 
     def lepton_connect(self) -> int :
-        lepton_sdk.LEP_OpenPort(1, lepton_type.LEP_CAMERA_PORT_E.LEP_CCI_TWI, 400, self._port)
+        lepton_sdk.LEP_OpenPort(1, lepton_type.LEP_CAMERA_PORT_E_TAG.LEP_CCI_TWI, 400, self._port)
         self._connected = True
         return 0
 
